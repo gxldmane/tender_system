f6e85226f96298fdf496b7220f7580e9030534d6
gxldmane <timofey.hodotaev@yandex.ru>
2024-06-24 17:26:03 +0500

    add example env

--- backend/app/Models/Tender.php
+++ backend/app/Models/Tender.php
@@ -8,10 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+
+
 class Tender extends Model
 {
     use HasFactory;
 
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'description',
@@ -23,31 +28,49 @@ class Tender extends Model
         'until_date',
     ];
 
+    /**
+     * @return BelongsTo
+     */
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
+    /**
+     * @return BelongsTo
+     */
     public function region(): BelongsTo
     {
         return $this->belongsTo(Region::class);
     }
 
+    /**
+     * @return HasMany
+     */
     public function files(): HasMany
     {
         return $this->hasMany(File::class);
     }
 
+    /**
+     * @return BelongsTo
+     */
     public function customer(): BelongsTo
     {
         return $this->belongsTo(User::class, 'customer_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder[]|Collection|Model[]|null[]|object[]
+     */
     public function executor()
     {
         return User::query()->where('id', $this->executor_id)->first()->get();
     }
 
+    /**
+     * @return HasMany
+     */
     public function bids(): HasMany
     {
         return $this->hasMany(Bid::class);
